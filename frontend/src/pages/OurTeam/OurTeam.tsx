b30c41e6b61cba143d37e5ae7ad40efa42763679
SusheelThapa <susheelthapa90@gmail.com>
2024-02-04 22:49:59 +0545

    setup route and layout for our-team page

---
+++ frontend/src/pages/OurTeam/OurTeam.tsx
@@ -0,0 +1,17 @@
+import Layout from "@/layout/Layout";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+
+const OurTeam = () => {
+  return (
+    <>
+      <Layout>
+        <NavBar />
+        <div>The Team Goes here.</div>
+      </Layout>
+      <Footer />
+    </>
+  );
+};
+
+export default OurTeam;
