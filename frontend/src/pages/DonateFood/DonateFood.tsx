9c16dfd362d99eebade30298d32ba796b12acec9
SusheelThapa <susheelthapa90@gmail.com>
2024-02-04 21:08:19 +0545

    create donate food form of donate food page

--- frontend/src/pages/DonateFood/DonateFood.tsx
+++ frontend/src/pages/DonateFood/DonateFood.tsx
@@ -1,5 +1,22 @@
+import DonateFoodForm from "@/components/DonateFoodForm";
+import DonateFoodHero from "@/components/DonateFoodHero";
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import Layout from "@/layout/Layout";
+
 const DonateFood = () => {
-  return <div>DonateFood</div>;
+  return (
+    <>
+      <Layout>
+        <NavBar />
+        <div className="grid grid-cols-2 gap-20 m-5 my-5 mb-16 px-8 py-8 h-[70vh] justify-center align-middle items-center">
+          <DonateFoodForm />
+          <DonateFoodHero />
+        </div>
+      </Layout>
+      <Footer />
+    </>
+  );
 };
 
 export default DonateFood;
