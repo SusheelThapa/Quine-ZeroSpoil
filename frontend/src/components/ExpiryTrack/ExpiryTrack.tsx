588ef3d30449bf47f3c7d96a3d93b26027ec7e83
Saurav Kumar Mahato <077bct079.saurav@pcampus.edu.np>
2024-02-05 00:59:49 +0545

    implemented alert feature, added icons, expiry in days feature

--- frontend/src/components/ExpiryTrack/ExpiryTrack.tsx
+++ frontend/src/components/ExpiryTrack/ExpiryTrack.tsx
@@ -10,15 +10,27 @@ import {
 } from '@/components/ui/table';
 
 import product_image from '@/assets/img/avatar/AB.png';
-
 import { useNavigate } from 'react-router-dom';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+
 
 interface Product {
     id: number;
     product_name: string;
     description: string;
-    expiry_date: string;
-    purchase_date: string;
+    expiry_date: Date;
+    purchase_date: Date;
     quantity: number;
     // Add other fields as needed
 }
@@ -37,23 +49,37 @@ const ExpiryTrack: React.FC = () => {
             .catch((error) => console.log(error));
     }, []);
 
-    const handleRemove = (deleteProduct:Product) => {
+    const handleRemove = (deleteProduct: Product) => {
         try {
-          // Make a delete request using Axios
-          axios
-            .delete(
-              `http://localhost:8000/api/expirytrackers/${deleteProduct.id}/`
-            )
-            .then(() =>
-              setProducts(products.filter((p:Product) => p.id !== deleteProduct.id))
-            )
-            .catch(() => {
-              console.log("error while in delete endpoint")
-            });
+            // Make a delete request using Axios
+            axios
+                .delete(
+                    `http://localhost:8000/api/expirytrackers/${deleteProduct.id}/`
+                )
+                .then(() =>
+                    setProducts(products.filter((p: Product) => p.id !== deleteProduct.id))
+                )
+                .catch(() => {
+                    console.log("error while in delete endpoint")
+                });
         } catch (error) {
-          console.error("Error deleting Project:", error);
+            console.error("Error deleting Project:", error);
         }
-      };
+    };
+
+
+
+    const handleDate = (startDate: Date, timeEnd: Date) => {
+        const newStartDate= new Date(startDate);
+        const newEndDate=new Date(timeEnd);
+        const one_day = 1000*60*60*24;
+        let result
+        result = Math.ceil((newEndDate.getTime()-newStartDate.getTime())/(one_day))
+        console.log('date Converter result', result)
+        if (result < 0 ) {return 0}
+        return result
+      }
+    
 
 
     return (
@@ -77,7 +103,6 @@ const ExpiryTrack: React.FC = () => {
                 <Table>
                     <TableHeader>
                         <TableRow>
-                            <TableHead className="w-[60px]"></TableHead>
                             <TableHead>Name</TableHead>
                             <TableHead>Description</TableHead>
                             <TableHead>Expiry Date</TableHead>
@@ -91,22 +116,37 @@ const ExpiryTrack: React.FC = () => {
                         {/* Loop through the products array */}
                         {products.map((product) => (
                             <TableRow key={product.id}>
-                                <TableCell>
-                                    <img className="" src={product_image} alt="product item image" />
-                                </TableCell>
+
                                 <TableCell className="">{product.product_name}</TableCell>
                                 <TableCell>{product.description}</TableCell>
-                                <TableCell>{product.expiry_date}</TableCell>
-                                <TableCell>{product.purchase_date}</TableCell>
+                                <TableCell>{product.expiry_date.toLocaleString()} (<span className='text-sm text-red-400 mx-1'>expires in {handleDate(product.purchase_date, product.expiry_date)} days</span>)</TableCell>
+                                <TableCell>{product.purchase_date.toLocaleString()}</TableCell>
                                 <TableCell>{product.quantity}</TableCell>
                                 <TableCell>
-                                    <button
-                                        type="submit"
-                                        onClick={()=> handleRemove(product)}
-                                        className="flex justify-center rounded-md bg-red-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg--500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                    >
-                                        remove
-                                    </button>
+
+
+                                    <AlertDialog>
+                                        <AlertDialogTrigger> <button
+                                            type="submit"
+                                            className="flex justify-center rounded-md bg-red-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg--500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                        >
+                                            remove
+                                        </button></AlertDialogTrigger>
+                                        <AlertDialogContent>
+                                            <AlertDialogHeader>
+                                                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                                                <AlertDialogDescription>
+                                                    This action cannot be undone. This will permanently delete your item.
+                                                </AlertDialogDescription>
+                                            </AlertDialogHeader>
+                                            <AlertDialogFooter>
+                                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                                <AlertDialogAction
+                                                onClick={() => handleRemove(product)} className='hover:bg-red-500'>Continue</AlertDialogAction>
+                                            </AlertDialogFooter>
+                                        </AlertDialogContent>
+                                    </AlertDialog>
+
                                 </TableCell>
 
                             </TableRow>
