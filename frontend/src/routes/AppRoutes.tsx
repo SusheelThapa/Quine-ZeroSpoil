f6fd8e5c48680ed280cac8ad941ba6222d4e50db
Saurav Kumar Mahato <077bct079.saurav@pcampus.edu.np>
2024-02-04 20:05:59 +0545

    made expiry date traking ui

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LandingPage from "@/pages/LandingPage";
+import ExpiryTrack from "@/components/ExpiryTrack";
+import AddItem from "@/components/ExpiryTrack/Additem";
+
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/expiry_track" element={<ExpiryTrack />} />
+        <Route path="/expiry_track/add_item" element={<AddItem />} />
+
       </Routes>
     </Router>
   );
