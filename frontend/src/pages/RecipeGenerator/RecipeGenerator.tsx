56c4246f8cef00ab99cd32fe9562241b51bf29a2
SusheelThapa <susheelthapa90@gmail.com>
2024-02-06 21:07:18 +0545

    implement circular loading animation

--- frontend/src/pages/RecipeGenerator/RecipeGenerator.tsx
+++ frontend/src/pages/RecipeGenerator/RecipeGenerator.tsx
@@ -4,10 +4,15 @@ import Footer from "@/components/Footer";
 
 import Recipe from "@/components/Recipe";
 import IngredientForm from "@/components/Ingredient/IngredientForm";
+import CircularLoader from "@/components/CircularLoader/CircularLoader";
+import { useState } from "react";
 
 const RecipeGenerator = () => {
+  const [loading, setLoading] = useState(false);
+
   const handleSubmit = (ingredients: string[]): void => {
     console.log(ingredients);
+    setLoading(true);
     // TODO: API CALL
   };
 
@@ -17,7 +22,7 @@ const RecipeGenerator = () => {
         <NavBar />
         <div className="grid grid-cols-2 my-20 h-[60vh] justify-center items-center align-middle">
           <IngredientForm onSubmit={handleSubmit} />
-          <Recipe />
+          {loading ? <CircularLoader /> : <Recipe />}
         </div>
       </Layout>
       <Footer />
